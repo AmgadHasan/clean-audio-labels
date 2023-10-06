554b7d02b864b0bc278c7903a092acd7c755d0db
AmgadHasan <eng.amghasan@gmail.com>
2023-10-06 21:49:08 +0300

    Create a function for arg parsing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request, Form, HTTPException
-#from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 import pandas as pd
 import base64
@@ -9,23 +8,45 @@ import wave
 import random
 import argparse
 
+# Create a FastAPI app
 app = FastAPI()
 
-# Define command-line arguments
-parser = argparse.ArgumentParser(description="Transcript Correction Web App")
-parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on")
-parser.add_argument("--file-path", type=str, default="test.parquet", help="Path to the Parquet file")
-args = parser.parse_args()
+def create_wav_in_memory(audio_data):
+    """
+    Create a WAV file in memory from audio data.
 
-# Create an instance of Jinja2Templates
-templates = Jinja2Templates(directory="templates")
+    Args:
+        audio_data (pd.Series): Audio data as a pandas Series.
 
-# Load your data here
-df = pd.read_parquet(args.file_path)
-indexes = iter(df.index)
-num_samples = df.shape[0]
+    Returns:
+        str: Base64-encoded WAV audio.
+    """
+    global SAMPLING_RATE
+    audio_data = (audio_data * 32767).astype(np.int16)
+    
+    # Create a WAV file in memory
+    wav_buffer = io.BytesIO()
+    with wave.open(wav_buffer, 'wb') as wf:
+        wf.setnchannels(1)  # Mono audio
+        wf.setsampwidth(2)  # 16-bit audio
+        wf.setframerate(SAMPLING_RATE)  # Sample rate (adjust as needed)
+        wf.writeframes(audio_data.tobytes())
+
+    # Encode the WAV data as base64
+    audio_base64 = base64.b64encode(wav_buffer.getvalue()).decode('utf-8')
+    
+    return audio_base64
 
 def get_validation_form(request):
+    """
+    Generate the validation form data for rendering in the template.
+
+    Args:
+        request (Request): The FastAPI request object.
+
+    Returns:
+        TemplateResponse: The HTML template response.
+    """
     # Randomly select a sample that hasn't been corrected yet
     index = next(indexes)
     
@@ -33,18 +54,7 @@ def get_validation_form(request):
 
     # Get the audio data from the DataFrame
     audio_data = df['audio'].iloc[index]
-    audio_data = (audio_data * 32767).astype(np.int16)
-
-    # Create a WAV file in memory
-    wav_buffer = io.BytesIO()
-    with wave.open(wav_buffer, 'wb') as wf:
-        wf.setnchannels(1)  # Mono audio
-        wf.setsampwidth(2)  # 16-bit audio
-        wf.setframerate(16000)  # Sample rate (adjust as needed)
-        wf.writeframes(audio_data.tobytes())
-
-    # Encode the WAV data as base64
-    audio_base64 = base64.b64encode(wav_buffer.getvalue()).decode('utf-8')
+    audio_base64 = create_wav_in_memory(audio_data)
 
     # Data to pass to the template
     template_data = {
@@ -59,30 +69,73 @@ def get_validation_form(request):
 
     return templates.TemplateResponse("validation_template.html", {"request": request, **template_data})
 
+def parse_arguments():
+    """
+    Parse command-line arguments and return an argparse.Namespace object.
+    """
+    parser = argparse.ArgumentParser(description="Transcript Correction Web App")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host to run the FastAPI app on")
+    parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on")
+    parser.add_argument("--file-path", type=str, default="test.parquet", help="Path to the Parquet file")
+    parser.add_argument("--templates", type=str, default="templates", help="Directory containing Jinja templates")
+    parser.add_argument("--sampling-rate", type=int, default=16_000, help="Sampling rate of the audio")
+    return parser.parse_args()
+
 
 
 # 'validate' endpoint
 @app.get("/")
 async def validate(request: Request):
+    """
+    Endpoint to render the validation form.
+
+    Args:
+        request (Request): The FastAPI request object.
+
+    Returns:
+        TemplateResponse: The HTML template response.
+    """
     response = get_validation_form(request)
     return response
-    
-
 
 # 'update' endpoint
 @app.post("/")
 async def update(request: Request, user_text: str = Form(...), index: int = Form(...)):
+    """
+    Endpoint to handle user text submissions and update the DataFrame.
+
+    Args:
+        request (Request): The FastAPI request object.
+        user_text (str): User-submitted text.
+        index (int): Index of the data row to update.
+
+    Returns:
+        TemplateResponse: The HTML template response.
+    """
     # Handle the user's submitted text and index here
     # For demonstration, we'll just print them
     print(f"Received text: {user_text}, index: {index}")
     df['corrected'].iloc[index] = user_text
-    print(df['corrected'].iloc[index])
-    df.to_parquet(file_path)
+    df.to_parquet(args.file_path)
     response = get_validation_form(request)
 
     return response
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=args.port)
+	import uvicorn
+    
+    # Parse command-line arguments
+	args = parse_arguments()
+	SAMPLING_RATE = args.sampling_rate
+
+	# Create an instance of Jinja2Templates
+	templates = Jinja2Templates(directory=args.templates)
+
+	# Load your data here
+	df = pd.read_parquet(args.file_path)
+
+	indexes = iter(df.index)
+	num_samples = df.shape[0]
+	
+	uvicorn.run(app, host=args.host, port=args.port)
 
