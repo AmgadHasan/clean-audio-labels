b0ff58a940a4d0d5890310c4a8a1070c135a7751
AmgadHasan <eng.amghasan@gmail.com>
2023-10-06 21:36:29 +0300

    Created jinja template for html

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 from fastapi import FastAPI, Request, Form, HTTPException
-from fastapi.responses import HTMLResponse
+#from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import pandas as pd
 import base64
 import io
 import numpy as np
 import wave
 import random
+import argparse
 
 app = FastAPI()
 
-file_path="test.parquet"
+# Define command-line arguments
+parser = argparse.ArgumentParser(description="Transcript Correction Web App")
+parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on")
+parser.add_argument("--file-path", type=str, default="test.parquet", help="Path to the Parquet file")
+args = parser.parse_args()
 
-df = pd.read_parquet(file_path)
+# Create an instance of Jinja2Templates
+templates = Jinja2Templates(directory="templates")
+
+# Load your data here
+df = pd.read_parquet(args.file_path)
 indexes = iter(df.index)
 num_samples = df.shape[0]
 
-def get_validation_form():
-    # Randomly a sample that hasn't been corrected yet
-    #index = random.choice(df[df['corrected'] != "<blank>"].index)
+def get_validation_form(request):
+    # Randomly select a sample that hasn't been corrected yet
     index = next(indexes)
-    #index = random.choice(df.index)
     
     num_completed = df[df['corrected'] != "<blank>"].shape[0]
 
     # Get the audio data from the DataFrame
     audio_data = df['audio'].iloc[index]
-
     audio_data = (audio_data * 32767).astype(np.int16)
 
     # Create a WAV file in memory
@@ -39,62 +46,43 @@ def get_validation_form():
     # Encode the WAV data as base64
     audio_base64 = base64.b64encode(wav_buffer.getvalue()).decode('utf-8')
 
+    # Data to pass to the template
+    template_data = {
+        "num_completed": num_completed,
+        "num_samples": num_samples,
+        "audio_base64": audio_base64,
+        "utterance": df['Utterance'].iloc[index],
+        "translation": df['translation'].iloc[index],
+        "corrected": df['corrected'].iloc[index],
+        "index": index,
+    }
 
-    # HTML form for the 'validate' endpoint
-    validate_form = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Validate Endpoint</title>
-    </head>
-    <body>
-        <h1>Validate Endpoint</h1>
-        <h2>Completed so far: {num_completed}/{num_samples}</h2>
-        <audio controls>
-            <source src="data:audio/wav;base64,{audio_base64}" type="audio/wav">
-            Your browser does not support the audio element.
-        </audio>
-        <p>Utterance: {df['Utterance'].iloc[index]}</p>
-        <p>Translation: {df['translation'].iloc[index]}</p>
-        <p>Corrected: {df['corrected'].iloc[index]}</p>
-        <p>Index: {index}</p>
-        <form action="/" method="post">
-            <label for="user_text">Enter Text:</label>
-            <!-- Pre-fill the text box with translation_value -->
-            <input type="text" id="user_text" name="user_text" required style="width: 70%;" value="{df['corrected'].iloc[index]}">
-            <input type="hidden" name="index" value="{index}">
-            <input type="submit" value="Submit">
-        </form>
-    </body>
-    </html>
-    """
-
-    return HTMLResponse(content=validate_form)
+    return templates.TemplateResponse("validation_template.html", {"request": request, **template_data})
 
 
 
 # 'validate' endpoint
-@app.get("/", response_class=HTMLResponse)
-async def validate():
-    response = get_validation_form()
+@app.get("/")
+async def validate(request: Request):
+    response = get_validation_form(request)
     return response
     
 
 
 # 'update' endpoint
 @app.post("/")
-async def update(user_text: str = Form(...), index: int = Form(...)):
+async def update(request: Request, user_text: str = Form(...), index: int = Form(...)):
     # Handle the user's submitted text and index here
     # For demonstration, we'll just print them
     print(f"Received text: {user_text}, index: {index}")
     df['corrected'].iloc[index] = user_text
     print(df['corrected'].iloc[index])
     df.to_parquet(file_path)
-    response = get_validation_form()
+    response = get_validation_form(request)
 
     return response
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=args.port)
 
